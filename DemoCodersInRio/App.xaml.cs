b221d3b02ab4f8c698ef48433be56fbddbf0bb5d
julianocustodio <juliano.custodio@hotmail.com.br>
2019-12-05 21:47:28 -0300

    App center demo

--- DemoCodersInRio/App.xaml.cs
+++ DemoCodersInRio/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +19,23 @@ namespace DemoCodersInRio
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+
+            AppCenter.Start("android=bd6f0f15-6bcc-4b07-944e-b9f3cc14ce26;" +
+                  "ios=d8ebaddc-426b-40b1-a7a0-bbdf52d2a8b1;",
+                  typeof(Analytics), typeof(Crashes));
+
+            try
+            {
+                throw new Exception("Erro proposital");
+            }
+            catch (Exception ex)
+            {
+                var properties = new Dictionary<string, string>
+                {
+                    { "ErrorMessage", ex.Message }
+                };
+                Crashes.TrackError(ex, properties);
+            }
         }
 
         protected override void OnSleep()
